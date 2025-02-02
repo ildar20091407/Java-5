c980f98d794d526d9c8c701f209df46e7357bd8d
i <i>
2025-02-02 17:35:01 +0500

    Java-5

---
+++ styles.css/main.js
@@ -0,0 +1,24 @@
+ alert(" Задание 1");
+
+ console.log(" Задание 1");
+ 
+ console.log("Привет !");
+
+ alert("Привет !");
+
+
+ for(i =0; i<=10;i++){
+    let user ={
+       usernumber: alert('Пользователь - ' + i),
+       login: prompt('введите имя'), 
+       age: +prompt('введите ваш возрост')
+ }
+ for (let key in user){
+    console.log(user[key]);
+   
+ }
+ }
+
+
+
+  
