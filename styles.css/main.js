9900a4f87abe0a882474e547fadd1b239c2c40f0
i <i>
2025-02-06 13:28:36 +0500

    j

--- styles.css/main.js
+++ styles.css/main.js
@@ -7,18 +7,18 @@
  alert("Привет !");
 
 
- for(i =0; i<=10;i++){
-    let user ={
-       usernumber: alert('Пользователь - ' + i),
-       login: prompt('введите имя'), 
-       age: +prompt('введите ваш возрост')
- }
- for (let key in user){
-    console.log(user[key]);
+ 
+
+
+
+   for(i =1; i<=10;i++){
+       user =[
+         usernumber = alert('Пользователь - ' + i),
+         login = prompt('введите имя'), 
+         age= +prompt('введите ваш возрост')
+      ]
+   }
+   console.log(user)
+      
    
- }
- }
 
-
-
-  
